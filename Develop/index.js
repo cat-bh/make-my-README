a787b5adc14473eca13a4f26ea5e066645e89ab3
Catherine <catheneghan@gmail.com>
2021-08-28 16:10:13 -0700

    wrote question array, Included packages needed at the beginning of index.js. Prompted questions in init() function that get sent to generateMarkdown.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter your title!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project!'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please input any installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage instructions. How is your project meant to be used?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can people contribute to your project? What are the contribution guidelines?'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Enter instructions for testing the application: '
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'Enter any additional information about contacting you:'
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like for this project?',
+        choices: ['MIT', 'GPLv3', 'Unlicense', 'None']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(path.join('./dist', fileName), data, err => {
+        if (err) {
+
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('Welcome to the README generator! Please answer all the following questions for the best quality README');
+    inquirer.prompt(questions)
+        .then(responses => {
+            console.log(responses);
+            generateMarkdown(responses);
+        })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,6 +12,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log("I'm in the different function!");
   return `# ${data.title}
 
 `;
